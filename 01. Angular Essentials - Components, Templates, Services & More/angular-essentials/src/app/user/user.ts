0cd1c9f25bdeb766d6d7c5f04dc03b7d6a91d965
hishamrehab <hishamrehab30@gmail.com>
2026-03-25 20:41:45 +0200

    Signals

--- 01. Angular Essentials - Components, Templates, Services & More/angular-essentials/src/app/user/user.ts
+++ 01. Angular Essentials - Components, Templates, Services & More/angular-essentials/src/app/user/user.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component , signal , computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-    const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length)
+   const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length)
 
    @Component({
     selector: 'app-user',
@@ -11,16 +11,15 @@ import { DUMMY_USERS } from '../dummy-users';
   })
 
 export class User {
-  selectedUser =  DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
-
- get imagePath() {
-  return 'assets/users/' + this.selectedUser.avatar;
- }
+//  get imagePath() {
+//   return 'assets/users/' + this.selectedUser().avatar;
+//  }
    
  onSelectUser() {
-   const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length)
-   this.selectedUser = DUMMY_USERS[randomIndex];
+   const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length);
+   this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
-
 }
