58a2feef91ab3c4c34e2d8cad6c55783390b0703
hishamrehab <hishamrehab30@gmail.com>
2026-03-26 17:44:04 +0200

    Accepting Objects As Inputs

--- 01. Angular Essentials - Components, Templates, Services & More/angular-essentials/src/app/app.component.ts
+++ 01. Angular Essentials - Components, Templates, Services & More/angular-essentials/src/app/app.component.ts
@@ -2,20 +2,27 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { User } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from "./tasks/tasks";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, User],
+  imports: [HeaderComponent, User, Tasks],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 
 export class AppComponent {
   users = DUMMY_USERS;
+   selectedUserId = "u1";
+
+
+ get selectedUser() {
+  return this.users.find(user => user.id === this.selectedUserId)!;
+ }
 
     onSelectUser(id: string){
-      console.log("Selected user with id: " + id);
-      }
+      this.selectedUserId = id;
     }
+}
   
