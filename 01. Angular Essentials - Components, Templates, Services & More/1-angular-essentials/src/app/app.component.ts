7a71da8dea94496abf93d19949856b91b75fff5a
hishamrehab <hishamrehab30@gmail.com>
2026-03-27 03:13:42 +0200

    Dynamic CSS Styling With Class Binding

--- 01. Angular Essentials - Components, Templates, Services & More/1-angular-essentials/src/app/app.component.ts
+++ 01. Angular Essentials - Components, Templates, Services & More/1-angular-essentials/src/app/app.component.ts
@@ -3,13 +3,12 @@ import { HeaderComponent } from './header/header.component';
 import { User } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
 import { Tasks } from "./tasks/tasks";
-import { NgFor ,NgIf } from '@angular/common';
-
+import { NgIf , NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, User, Tasks , NgFor , NgIf],
+  imports: [HeaderComponent, User, Tasks , NgIf , NgFor ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
