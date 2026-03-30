0cf7cf05be259e87f3dc7f7b169a7c6694292f33
hishamrehab <hishamrehab30@gmail.com>
2026-03-30 23:44:08 +0200

    Splitting App Component into Multiple Componenets

--- 05. Components & Templates/components-and-templates/src/app/app.component.ts
+++ 05. Components & Templates/components-and-templates/src/app/app.component.ts
@@ -1,41 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Header } from './header/header';
+import { ServerStatus } from './dashbaord/server-status/server-status';
+import { Traffic } from './dashbaord/traffic/traffic';
+import { Tickets } from './dashbaord/tickets/tickets';
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [Header, ServerStatus, Traffic, Tickets],
   templateUrl: './app.component.html',
 })
+
 export class AppComponent {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
+ 
 }
