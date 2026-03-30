ddaf81cfc8bda8925f4c1404b50075f6f9ac7b5d
hishamrehab <hishamrehab30@gmail.com>
2026-03-30 03:48:49 +0200

    Outputtung Results in a table

--- 03. Angular Essentials - Time To Practice/src/app/app.component.ts
+++ 03. Angular Essentials - Time To Practice/src/app/app.component.ts
@@ -1,33 +1,39 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
+import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-   calculateInvestmentResults(data : {
-      initialInvestment : number,
-      annualInvestment : number,
-      expectedReturn : number, 
-      duration : number
-   }) {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
+   onCalculateInvestmentResults(data : InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
     console.log(initialInvestment, annualInvestment, expectedReturn, duration);
   const annualData = [];
   let investmentValue = initialInvestment;
 
-  for (let i = 0; i < duration; i++) {
+  for (let i = 0; i < duration; i++) { 
     const year = i + 1;
     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
     investmentValue += interestEarnedInYear + annualInvestment;
     const totalInterest =
       investmentValue - annualInvestment * year - initialInvestment;
-    annualData.push({
+      annualData.push({
       year: year,
       interest: interestEarnedInYear,
       valueEndOfYear: investmentValue,
@@ -37,7 +43,6 @@ export class AppComponent {
     });
   }
 
-  return annualData;
-}
-
+this.resultsData = annualData;
+  }
 }
