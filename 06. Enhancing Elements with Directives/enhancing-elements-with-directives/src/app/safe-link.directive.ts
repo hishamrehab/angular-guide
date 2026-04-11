10eb698722648b28261b576f984a4de3b844e52c
hishamrehab <hishamrehab30@gmail.com>
2026-04-11 11:24:35 +0200

    Directives

--- 06. Enhancing Elements with Directives/enhancing-elements-with-directives/src/app/safe-link.directive.ts
+++ 06. Enhancing Elements with Directives/enhancing-elements-with-directives/src/app/safe-link.directive.ts
@@ -1,24 +1,33 @@
-import { Directive } from "@angular/core";
+import { Directive  , input , inject , ElementRef } from "@angular/core";
+import { LogDirective } from "./log.directive";
+
 
 @Directive({
   selector: 'a[appSafeLink]',
-  standalone: true,
+  standalone: true, 
   host: {
     '(click)': 'onConfirmLeavePage($event)'
-  }
+  },
+  hostDirectives: [LogDirective]
 })
 
 export class SafeLinkDirective {
-    constructor() {
-        console.log('SafeLinkDirective is active');
-    }
+  queryParam = input('myapp' , {alias: 'appSafeLink'});
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
 
-     onConfirmLeavePage(event: MouseEvent) {
-     const wantsToLeave = window.confirm("Do you want to leave the app?");
-    if (wantsToLeave) {
-      return;
-    }
-
-    event.preventDefault();
+  constructor() {
+    console.log('SafeLinkDirective is active');
   }
+
+  onConfirmLeavePage(event: MouseEvent) {
+  const wantsToLeave = window.confirm("Do you want to leave the app?");
+
+  if(wantsToLeave) {
+    const address = this.hostElementRef.nativeElement.href;
+    this.hostElementRef.nativeElement.href = address + '?from='  + this.queryParam();
+    return;
+  }
+
+  event?.preventDefault();
+}
 };
